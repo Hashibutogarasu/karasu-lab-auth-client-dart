bdd354ec5cb60adb09fe93fe92a6a813af6865de
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-02-07 23:28:57 +0900

    初回のリリースに向けた設定ファイルとモデルの追加

---
+++ lib/src/serializers.dart
@@ -0,0 +1,48 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+
+// ignore_for_file: unused_import
+
+import 'package:one_of_serializer/any_of_serializer.dart';
+import 'package:one_of_serializer/one_of_serializer.dart';
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/json_object.dart';
+import 'package:built_value/serializer.dart';
+import 'package:built_value/standard_json_plugin.dart';
+import 'package:built_value/iso_8601_date_time_serializer.dart';
+import 'package:karasu256_api_auth_client/src/date_serializer.dart';
+import 'package:karasu256_api_auth_client/src/model/date.dart';
+
+import 'package:karasu256_api_auth_client/src/model/auth_controller_change_password_request.dart';
+import 'package:karasu256_api_auth_client/src/model/auth_controller_disable_mfa_request.dart';
+import 'package:karasu256_api_auth_client/src/model/auth_controller_enable_mfa_request.dart';
+import 'package:karasu256_api_auth_client/src/model/auth_controller_forgot_password_request.dart';
+import 'package:karasu256_api_auth_client/src/model/auth_controller_refresh_token_request.dart';
+import 'package:karasu256_api_auth_client/src/model/auth_controller_set_up_mfa_request.dart';
+import 'package:karasu256_api_auth_client/src/model/auth_controller_signin_confirm_request.dart';
+import 'package:karasu256_api_auth_client/src/model/auth_controller_signin_request.dart';
+import 'package:karasu256_api_auth_client/src/model/auth_controller_signup_request.dart';
+
+part 'serializers.g.dart';
+
+@SerializersFor([
+  AuthControllerChangePasswordRequest,
+  AuthControllerDisableMfaRequest,
+  AuthControllerEnableMfaRequest,
+  AuthControllerForgotPasswordRequest,
+  AuthControllerRefreshTokenRequest,
+  AuthControllerSetUpMfaRequest,
+  AuthControllerSigninConfirmRequest,
+  AuthControllerSigninRequest,
+  AuthControllerSignupRequest,
+])
+Serializers serializers = (_$serializers.toBuilder()
+      ..add(const OneOfSerializer())
+      ..add(const AnyOfSerializer())
+      ..add(const DateSerializer())
+      ..add(Iso8601DateTimeSerializer()))
+    .build();
+
+Serializers standardSerializers =
+    (serializers.toBuilder()..addPlugin(StandardJsonPlugin())).build();
---
+++ lib/src/serializers.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'serializers.dart';
+
+// **************************************************************************
+// BuiltValueGenerator
+// **************************************************************************
+
+Serializers _$serializers = (new Serializers().toBuilder()
+      ..add(AuthControllerChangePasswordRequest.serializer)
+      ..add(AuthControllerDisableMfaRequest.serializer)
+      ..add(AuthControllerEnableMfaRequest.serializer)
+      ..add(AuthControllerEnableMfaRequestAnswerChallengeEnum.serializer)
+      ..add(AuthControllerForgotPasswordRequest.serializer)
+      ..add(AuthControllerRefreshTokenRequest.serializer)
+      ..add(AuthControllerSetUpMfaRequest.serializer)
+      ..add(AuthControllerSigninConfirmRequest.serializer)
+      ..add(AuthControllerSigninRequest.serializer)
+      ..add(AuthControllerSignupRequest.serializer))
+    .build();
+
+// ignore_for_file: deprecated_member_use_from_same_package,type=lint
